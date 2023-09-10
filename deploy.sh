3eb61727663057f8df28a07a8a816728e3c08d0a
BossXiang <bosszheng220@gmail.com>
2023-09-10 18:42:27 +0800

    Update docker-compose.yml

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+docker rmi kickstart-frontend kickstart-backend
+docker build . -t kickstart-backend
+docker save kickstart-backend -o kickstart-backend.tar
+scp kickstart-backend.tar ubuntu@35.234.63.181:tailor-bliss
